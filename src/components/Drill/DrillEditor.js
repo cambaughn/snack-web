fba8a182a61e238b13729421e75e67aa9c937149
Cameron Baughn <cam.baughn@gmail.com>
2018-10-24 19:34:28 -0700

    Add full drill editor

--- src/components/Drill/DrillEditor.js
+++ src/components/Drill/DrillEditor.js
@@ -10,50 +10,101 @@ class DrillEditor extends Component {
     this.state = {
       spanishText: this.props.question.spanish_text || '',
       englishText: this.props.question.english_text || '',
+      spanishAnswers: this.props.question.spanish_answers || [],
+      englishAnswers: this.props.question.english_answers || [],
       saved: true,
     }
-
-    console.log('rendering')
   }
 
-  handleChange = (field, value) => {
-    console.log(field, value)
-    this.setState({ field: value });
+  handleMainAnswerChange = (field, value) => {
+    let stateObject = { saved: false };
+    stateObject[field] = value;
+    this.setState(stateObject);
   }
 
   handleSave = () => {
     db.collection('questions').doc(this.props.question.id).set({
       spanish_text: this.state.spanishText,
-      english_text: this.state.englishText
+      english_text: this.state.englishText,
+      spanish_answers: this.state.spanishAnswers,
+      english_answers: this.state.englishAnswers,
     }, { merge: true })
     .then(() => {
       this.setState({ saved: true })
     });
   }
 
+  handleAnswersChange = (value, index, answers) => {
+    console.log(value, index);
+    let newAnswers = this.state[answers].slice();
+    newAnswers[index] = value;
+    let stateObject = { saved: false };
+    stateObject[answers] = newAnswers;
+    this.setState(stateObject);
+  }
+
+
   render() {
+    let saveText = this.state.saved ? 'Saved' : 'Save';
     return (
       <div style={styles.container}>
-        <button type="button" style={styles.saveButton} onClick={this.handleSave}>Save</button>
+        <div style={styles.buttonWrapper}>
+          <button type="button" style={styles.saveButton} onClick={this.handleSave}>{saveText}</button>
+        </div>
+        <div style={styles.languageHeaderWrapper}>
+          <h3 style={styles.languageHeader}>Spanish</h3>
+          <h3 style={styles.languageHeader}>English</h3>
+        </div>
+
         <div style={styles.mainAnswersWrapper}>
           <div>
-            <h4>Spanish</h4>
             <textarea
               value={this.state.spanishText}
-              onChange={e => this.setState({ spanishText: e.target.value})}
+              onChange={e => this.handleMainAnswerChange('spanishText', e.target.value)}
               style={styles.textarea}
             />
           </div>
 
           <div>
-            <h4>English</h4>
             <textarea
               value={this.state.englishText}
-              onChange={e => this.setState({ englishText: e.target.value})}
+              onChange={e => this.handleMainAnswerChange('englishText', e.target.value)}
               style={styles.textarea}
             />
           </div>
         </div>
+
+        <div style={styles.centeredText}>
+          <h4>More answers</h4>
+        </div>
+        <div style={styles.moreAnswers}>
+
+          <div style={styles.moreAnswersList}>
+            { this.state.spanishAnswers.map((answer, i) => (
+              <textarea
+                value={this.state.spanishAnswers[i]}
+                onChange={e => this.handleAnswersChange(e.target.value, i, 'spanishAnswers') }
+                style={styles.textarea}
+                key={i}
+              />
+            )
+          )}
+
+          </div>
+
+          <div style={styles.moreAnswersList}>
+            { this.state.englishAnswers.map((answer, i) => (
+              <textarea
+                value={this.state.englishAnswers[i]}
+                onChange={e => this.handleAnswersChange(e.target.value, i, 'englishAnswers') }
+                style={styles.textarea}
+                key={i}
+              />
+            )
+          )}
+          </div>
+        </div>
+
       </div>
     );
   }
@@ -73,23 +124,25 @@ const styles = {
     paddingTop: 50,
   },
 
+  languageHeaderWrapper: {
+    width: 775,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+  },
+
+  languageHeader: {
+    width: 350,
+  },
+
   mainAnswersWrapper: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'flex-start',
     justifyContent: 'space-between',
 
-    width: 800,
-  },
-
-  input: {
-    border: 'none',
-    outline: 'none',
-
-    fontSize: '1.3em',
-    // fontWeight: 'bold',
-
-    marginBottom: 30,
+    width: 775,
   },
 
   textarea: {
@@ -97,18 +150,33 @@ const styles = {
     height: '100%',
     boxSizing: 'border-box',
 
-    marginBottom: 40,
-    marginRight: 40,
+    marginBottom: 30,
     paddingRight: 20,
 
     fontSize: '1.1em',
 
     lineHeight: 2,
 
-    border: 'none',
+    border: '1px solid #dfe6e9',
     overflow: 'auto',
     outline: 'none',
     resize: 'none',
+  },
+
+  centeredText: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonWrapper: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+
+    paddingRight: 50,
   },
 
   saveButton: {
@@ -131,7 +199,17 @@ const styles = {
     cursor: 'pointer',
   },
 
+  moreAnswers: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
 
+    width: 775,
+  },
+
+  moreAnswersList: {
+  },
 }
 
 export default DrillEditor;
